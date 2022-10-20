e6ca5b15ece43657242fa11b0b8991018471348f
Ailton Ferreira Luz <djailtim@gmail.com>
2022-10-20 15:47:38 -0400

    Inserido projeto java sobre tratamento de exceções

---
+++ tratamento-excecoes/src/TryCatch.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            Integer idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            Double altura = scanner.nextDouble();
+
+            System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome + ".");
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm.");
+            scanner.close();
+        } catch (InputMismatchException e) {
+            System.err.println("Os campos idade e altura precisam ser numéricos");
+        }
+    }
+}
